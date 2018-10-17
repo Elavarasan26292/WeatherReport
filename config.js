245752b35e4b13b9c05d59ed57ef30ad93ff4b83
Elavarasan26292 <elavarasan26292@gmail.com>
2018-10-17 10:17:11 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const config={
       weather:{
-        url_paris:"http://api.openweathermap.org/data/2.5/forecast?q=Paris,Fr&APPID=",
+        url_paris:"http://api.openweathermap.org/data/2.5010/forecast?q=Paris,Fr&APPID=010",
         key:"bc70465154a8da390b8f387d6f51b312"
       }
 }
